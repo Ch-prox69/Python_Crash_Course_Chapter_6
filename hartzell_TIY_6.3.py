16458226df851e2a6a3d2504a0e79db9cf240bc7
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-09-10 13:10:24 -0400

    Add files via upload

---
+++ hartzell_TIY_6.3.py
@@ -0,0 +1,12 @@
+programming_words = {
+    'list': 'Collection of items in a strict order',
+    'value': 'A letter or number in a program',
+    'boolean': 'A value that is either true or false',
+    'string': 'A series of characters',
+    'dictionary': 'A collection of key-value pairs',
+}
+
+# Printing the definitions and values
+for word, meaning in programming_words.items():
+    print(f"{word.title()}:")
+    print(f"{meaning.title()}\n")
