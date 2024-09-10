6087bfe2ca70310c72575437ac0ebebc9c120a95
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-09-10 12:29:08 -0400

    Add files via upload

---
+++ hartzell_TIY_6.1.py
@@ -0,0 +1,92 @@
+people_known = {
+    'first_name1': 'jon',
+    'last_name1': 'becraft',
+    'age1': '17',
+    'city1': 'tanbark trails, Fort Wayne',
+
+    'first_name2': 'peyton',
+    'last_name2': 'moses',
+    'age2': '17',
+    'city2': 'sunny brook, Fort Wayne',
+
+    'first_name3': 'aiden',
+    'last_name3': 'melnick',
+    'age3': '17',
+    'city3': 'blue lagoon, Fort Wayne',
+
+    'first_name4': 'quinn',
+    'last_name4': 'deshone',
+    'age4': '18',
+    'city4': 'junkyard avenue, Fort Wayne',
+
+    'first_name5': 'jacob',
+    'last_name5': 'reynolds',
+    'age5': '17',
+    'city5': 'park avenue, Fort Wayne',
+
+    'first_name6': 'daniel',
+    'last_name6': 'webber',
+    'age6': '17',
+    'city6': 'misty mountains, Fort Wayne',
+}
+
+# Printing the people in the dictionary
+# Jon Becraft
+firstname1 = people_known['first_name1'].title()
+print(f"The fist name of person one is {firstname1}")
+lastname1 = people_known['last_name1'].title()
+print(f"The last name of person one is {lastname1}")
+age1 = people_known['age1'].title()
+print(f"The age of person one is {age1}")
+city1 = people_known['city1'].title()
+print(f"The city where person one lives in is {city1}\n")
+
+# Peyton Moses
+firstname2 = people_known['first_name2'].title()
+print(f"The first name of person two is {firstname2}")
+lastname2 = people_known['last_name2'].title()
+print(f"The last name of person two is {lastname2}")
+age2 = people_known['age2'].title()
+print(f"The age of person two is {age2}")
+city2 = people_known['city2'].title()
+print(f"The city where person two lives is {city2}\n")
+
+# Aiden Melnick
+firstname3 = people_known['first_name3'].title()
+print(f"The first name of person three is {firstname3}")
+lastname3 = people_known['last_name3'].title()
+print(f"The last name of person three is {lastname3}")
+age3 = people_known['age3'].title()
+print(f"The age of person three is {age3}")
+city3 = people_known['city3'].title()
+print(f"The city where person three lives is {city3}\n")
+
+# Quinn Deshone
+firstname4 = people_known['first_name4'].title()
+print(f"The first name of person four is {firstname4}")
+lastname4 = people_known['last_name4'].title()
+print(f"The last name of person three is {lastname4}")
+age4 = people_known['age4'].title()
+print(f"The age of person four is {age4}")
+city4 = people_known['city4'].title()
+print(f"The city where person four lives is {city4}\n")
+
+# Jacob Reynolds
+firstname5 = people_known['first_name5'].title()
+print(f"The first name of person five is {firstname5}")
+lastname5 = people_known['last_name5'].title()
+print(f"The last name of person five is {lastname5}")
+age5 = people_known['age5'].title()
+print(f"The age of person five is {age5}")
+city5 = people_known['city5'].title()
+print(f"The city where person five lives is {city5}\n")
+
+# Daniel Webber
+firstname6 = people_known['first_name6'].title()
+print(f"The first name of person six is {firstname6}")
+lastname6 = people_known['last_name6'].title()
+print(f"The last name of person six is {lastname6}")
+age6 = people_known['age6'].title()
+print(f"The age of person six is {age6}")
+city6 = people_known['city6'].title()
+print(f"The city where person six lives is {city6}\n")
