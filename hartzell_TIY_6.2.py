5147b8705f7b4febd2be41faf9583f897fea813c
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-09-10 12:51:54 -0400

    Add files via upload

---
+++ hartzell_TIY_6.2.py
@@ -0,0 +1,27 @@
+names_and_numbers = {
+    'name1': 'jon', 'jon_fav_num': '69',
+    'name2': 'daniel', 'dan_fav_num': '22',
+    'name3': 'peyton', 'pey_fav_num': '47',
+    'name4': 'quinn', 'qui_fav_num': '18',
+    'name5': 'jacob', 'jac_fav_num': '9',
+}
+
+# Assigning the keys to values
+name1 = names_and_numbers['name1'].title()
+number1 = names_and_numbers['jon_fav_num']
+name2 = names_and_numbers['name2'].title()
+number2 = names_and_numbers['dan_fav_num']
+name3 = names_and_numbers['name3'].title()
+number3 = names_and_numbers['pey_fav_num']
+name4 = names_and_numbers['name4'].title()
+number4 = names_and_numbers['qui_fav_num']
+name5 = names_and_numbers['name5'].title()
+number5 = names_and_numbers['jac_fav_num']
+
+# Printing the values
+print(f"{name1}s favorite number is {number1}\n")
+print(f"{name2}s favorite number is {number2}\n")
+print(f"{name3}s favorite number is {number3}\n")
+print(f"{name4}s favorite number is {number4}\n")
+print(f"{name5}s favorite number is {number5}\n")
+
